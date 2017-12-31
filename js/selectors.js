1dca2bc0b87d2ebafabba5be9f2ab2d39b07f668
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 17:52:47 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,18 +1,18 @@
-// declare your functions here...
-$('div')            // element selector
-$('.pics')          // class selector
-$('#baby-ninja')    // id selector
-$('ul li')          // descendant selector
-
-$('parent-tag child-tag:first-child')       // first child
-$('div img:first-child')                    //
-$("img[alt='the beatles making faces']")    // alt tag selector
-$('div:last')                               // last selector
-
-$(document).ready(function() {              // document ready!!!
-
-})
-
+// // declare your functions here...
+// $('div')            // element selector
+// $('.pics')          // class selector
+// $('#baby-ninja')    // id selector
+// $('ul li')          // descendant selector
+// 
+// $('parent-tag child-tag:first-child')       // first child
+// $('div img:first-child')                    //
+// $("img[alt='the beatles making faces']")    // alt tag selector
+// $('div:last')                               // last selector
+// 
+// $(document).ready(function() {              // document ready!!!
+// 
+// })
+          // lesson code here
 function paragraphSelector() {
   return $('p')
 }
